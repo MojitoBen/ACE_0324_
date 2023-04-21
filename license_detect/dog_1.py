ea55539bff4241c42c8bbdba1fd105659e21d1fd
mochicomame <124657177+MojitoBen@users.noreply.github.com>
2023-04-21 14:55:27 +0800

    Delete dog_1.py

--- license_detect/dog_1.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-import time
-while True:
-    try:  
-        subprocess.call(r'python C:\Users\Asc-user\Documents\YOLO\darknet\main\AI_1.py')
-    except:
-        pass
-
